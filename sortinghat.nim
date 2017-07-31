60ce4197bb05432ad8770cbc490f996234756a5d
Sergey Bochkov <bochkovsa@gmail.com>
2017-07-31 07:13:24 +0500

    directory support

--- sortinghat.nim
+++ sortinghat.nim
@@ -1,39 +1,42 @@
 import os
-import strutils
 import osproc
+import strutils
 
-proc move(name: string, hash: string) =
-  echo "name=", name
-  echo "hash=", hash
+proc move(hash: string, dir: string, name: string) =
+  echo "hash=$1, dir=$2, name=$3" % [hash, dir, name]
   var settings: string
-  settings = readFile("settings.ini")
+  settings = readFile("$1/settings.ini" % getAppDir())
   var row: seq[string]
   for i in settings.split("\n"):
-    if not i.startsWith("#"):
+    if i != "" and not i.startsWith("#"):
       row = i.split("=")
       if name.startsWith(row[0]):
         if hash != "":
           discard 
-            startProcess(
+            execProcess(
               command = "transmission-remote", 
               args = ["--torrent", hash, "--remove"]
-            ).waitForExit()
-        moveFile(name, "$dir/$name" % ["dir", row[1], "name", name])
-        echo "moved $source to $dir/$name" % ["source", name, "dir", row[1], "name", name]
+            )
+        echo "try to move $1/$2 to $3/$4" % [dir, name, row[1], name]
+        moveFile(
+          "$1/$2" % [dir, name], 
+          "$1/$2" % [row[1], name]
+        )
+        echo "moved $1/$2 to $3/$4" % [dir, name, row[1], name]
         return
   echo "torrent=$1 ignored" % [name]
 
 proc move(name : string) =
-  move(name, "")
+  move("", name.splitFile().dir, name.splitFile().name)
 
 var
-  name: string
-  hash: string
-
+  hash, dir, name: string
+dir = os.getEnv("TR_TORRENT_DIR")
 name = os.getEnv("TR_TORRENT_NAME")
 hash = os.getEnv("TR_TORRENT_HASH")
+
 if name != "" and hash != "":
-  move(name, hash)
+  move(hash, dir, name)
 elif paramCount() > 0:
   move(paramStr(1))
 else:
