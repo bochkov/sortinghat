dc1f389ef964dc68ccbc93e2de85483d97695884
Sergey Bochkov <bochkovsa@gmail.com>
2017-12-03 09:29:59 +0500

    startProcess switched to execCmd

--- sortinghat.nim
+++ sortinghat.nim
@@ -7,38 +7,31 @@ proc move(hash: string, dir: string, name: string) =
   var settings: string
   settings = readFile("$1/settings.ini" % getAppDir())
   var row: seq[string]
+  var changed: bool = false
   for i in settings.split("\n"):
     if i != "" and not i.startsWith("#"):
       row = i.split("=")
       if name.startsWith(row[0]):
         if hash != "":
-          discard
-            startProcess(
-              command = "/usr/bin/transmission-remote",
-              args = ["--torrent", hash, "--remove"]
-            ).waitForExit()
+          discard execCmd("transmission-remote --torrent $1 --remove" % [hash])
         echo "try to move $1/$2 to $3/$4" % [dir, name, row[1], name]
-        discard
-          startProcess(
-              command = "/bin/mv",
-              args = ["$1/$2" % [dir, name], "$1/$2" % [row[1], name]]
-            ).waitForExit()
+        discard execCmd("mv $1/$2 $3/$4" % [dir, name, row[1], name])
         echo "moved $1/$2 to $3/$4" % [dir, name, row[1], name]
-        return
-  echo "torrent=$1 ignored" % [name]
+        changed = true
+        break
+  if not changed:
+    echo "torrent=$1 ignored" % [name]
 
-proc move(name : string) =
-  move("", name.splitFile().dir, name.extractFilename())
-
-var
-  hash, dir, name: string
-dir = os.getEnv("TR_TORRENT_DIR")
-name = os.getEnv("TR_TORRENT_NAME")
-hash = os.getEnv("TR_TORRENT_HASH")
-
-if name != "" and hash != "":
-  move(hash, dir, name)
-elif paramCount() > 0:
-  move(paramStr(1))
-else:
-  echo "Usage: sortinghat <name>"
+if isMainModule:
+  var hash, dir, name: string
+  dir = os.getEnv("TR_TORRENT_DIR")
+  name = os.getEnv("TR_TORRENT_NAME")
+  hash = os.getEnv("TR_TORRENT_HASH")
+  if name == "" and hash == "" and paramCount() == 0:
+    echo "Usage: sortinghat <name>"
+  else:
+    var file : string = paramStr(1)
+    dir = file.splitFile().dir
+    name = file.extractFilename()
+    move(hash, dir, name)
+    
