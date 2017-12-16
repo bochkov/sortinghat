b72e8c936dd8c07d09d91c2d527c4b8b530305a6
Sergey Bochkov <bochkovsa@gmail.com>
2017-12-16 12:08:58 +0500

    trying OOP in nim

--- sortinghat.nim
+++ sortinghat.nim
@@ -2,35 +2,88 @@ import os
 import osproc
 import strutils
 
-proc move(hash: string, dir: string, name: string) =
-  echo "hash=$1, dir=$2, name=$3" % [hash, dir, name]
-  var settings: string
-  settings = readFile("$1/settings.ini" % getAppDir())
-  var row: seq[string]
-  var changed: bool = false
-  for i in settings.split("\n"):
-    if i != "" and not i.startsWith("#"):
-      row = i.split("=")
-      if name.startsWith(row[0]):
-        if hash != "":
-          discard execCmd("transmission-remote --torrent $1 --remove" % [hash])
-        echo "try to move $1/$2 to $3/$4" % [dir, name, row[1], name]
-        discard execCmd("mv $1/$2 $3/$4" % [dir, name, row[1], name])
-        echo "moved $1/$2 to $3/$4" % [dir, name, row[1], name]
-        changed = true
-        break
-  if not changed:
-    echo "torrent=$1 ignored" % [name]
+type
+  Row = ref object of RootObj
+    line : string
 
-if isMainModule:
+  Source = ref object of RootObj
+    dir : string
+    file : string
+
+  Move = ref object of RootObj
+    source : string
+
+  StopTransmission = ref object of Move
+    hash : string
+    origin : Move
+
+  Target = ref object of Move
+    origin : Move
+
+proc newRow(line : string) : Row =
+  new result
+  result.line = line
+
+proc isEmpty(row : Row) : bool =
+  return row.line == "" or row.line.startsWith("#")
+
+proc match(row : Row, source : string) : bool = 
+  var elems : seq[string] = row.line.split("=")
+  return elems.len > 1 and source.startsWith(elems[0])
+
+proc toKV(row : Row) : tuple[key : string, dir : string] =
+  var elems : seq[string] = row.line.split("=")
+  if elems.len == 2:
+    return (elems[0], elems[1])
+  else:
+    raise newException(OSError, "Cannot get key-value")
+
+method move(this : Move, target : string) {.base.} =
+  echo "try to move $1 to $2" % [this.source, target]
+  discard execCmd("mv $1 $2" % [this.source, target])
+  echo "moved $1 to $2" % [this.source, target]
+
+method move(this : StopTransmission, target : string) =
+  if this.hash != "":
+    discard execCmd("transmission-remote --torrent $1 --remove" % this.hash)
+  this.origin.move(target)
+
+method move(this : Target, target : string = "") =
+    var settings : string = readFile("$1/settings.ini" % getAppDir())
+    var row : Row
+    for line in settings.split("\n"):
+      row = newRow(line)
+      if not row.isEmpty() and row.match(this.origin.source):
+        this.origin.move(row.toKV().dir)
+        break
+
+proc eval(this : Source) : string =
   var
-    hash : string = os.getEnv("TR_TORRENT_HASH")
-    name : string = os.getEnv("TR_TORRENT_NAME")
-    dir  : string = os.getEnv("TR_TORRENT_DIR")
-  if hash != "" and name != "" and dir != "":
-    move(hash, dir, name)
-  elif paramCount() > 0:
-    var file : string = paramStr(1)
-    move("", file.splitFile().dir, file.extractFilename())
+    hash : string = getEnv("TR_TORRENT_HASH")
+    name : string = getEnv("TR_TORRENT_NAME")
+    dir  : string = getEnv("TR_TORRENT_DIR")
+  if hash == "" and paramCount() > 0:
+    name = paramStr(1).extractFilename()
+    dir = paramStr(1).splitFile().dir
   else:
     echo "Usage: sortinghat <name>"
+    raise newException(OSError, "params not defined")
+  if dir == "" or dir == ".":
+    dir = getAppDir()
+  return "$1/$2" % [dir, name]
+
+if isMainModule:
+  try:
+    Target(
+      origin:
+        StopTransmission(
+          hash : getEnv("TR_TORRENT_HASH"),
+          origin:
+            Move(
+              source : 
+                Source().eval()
+            )
+        )
+    ).move()
+  except:
+    discard
